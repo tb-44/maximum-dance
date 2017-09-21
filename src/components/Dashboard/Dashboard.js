0e7045a2b234d1a7c9e84eb07ac218a7d57b6940
Trent Bennett <rehab.rtb@gmail.com>
2017-09-21 07:59:07 -0600

    revising reducer

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
-import { getParentInfo } from '../../ducks/reducer';
+import { getParentInfo, addParentInfo } from '../../ducks/reducer';
 import Header from './../Header/Header';
 import './Dashboard.css';
 
@@ -19,57 +19,26 @@ class Dashboard extends Component {
       phone: '',
       email: ''
     }
-    this.addNewParent = this.addNewParent.bind(this);
-    this.handleParentInfo = this.handleParentInfo.bind(this);
+
+    this.addParentInfo = this.addParentInfo.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount() {
-    axios.post('http://localhost:3005/api/').then(res => {
-      this.setState({
-        parents: res.data
-      })
-      console.log(this.state.parents)
-    }) 
+
+  componentDidMount(){
+    this.props.getParentInfo()
   }
 
-  // componentDidMount(){
-    // this.props.getParentInfo()
-
-    addNewParent() {
-      var parents = {
-        firstname: this.state.firstname,
-        lastname: this.state.lastname,
-        address: this.state.address,
-        city: this.state.city,
-        state: this.state.state,
-        zip: this.state.zip,
-        phone: this.state.phone,
-        email: this.state.email
-      }
-      axios.post('http://localhost:3005/api/parent').then(res => {
-        console.log(res.data)
-        this.setState({
-          parent: res.data
-          
-        })
-        // console.log()
-      })
+    addParentInfo() {
+      const { firstname, lastname, address, city, state, zip, phone, email } = this.state;
+      return addParentInfo({ firstname, lastname, address, city, state, zip, phone, email });
   }
 
-  handleParentInfo(e) {
-    this.setState({
-      firstname: e.target.value,
-      lastname: e.target.value,
-      address: e.target.value,
-      city: e.target.value,
-      state: e.target.value,
-      zip: e.target.value,
-      phone: e.target.value,
-      email: e.target.value
+  // handleParentInfo(e) {
+  //   this.setState({
 
-    });
-  }
+  //   });
+  // }
 
   handleSubmit(e) {
     e.preventDefault();
@@ -96,124 +65,71 @@ class Dashboard extends Component {
 						<label >First Name:</label>
 
 						<input id='formFirstName' className='form-input' type='text' 
-            onChange={this.handleParentInfo} value={this.state.firstname} />
+            onChange={this.addParentInfo} value={this.state.firstname} />
 					</fieldset>
 
 					<fieldset className='form-group'>
 						<label>Last Name:</label>
 
 						<input id='formLastName' className='form-input' type='text' 
-            onChange={(e) => this.state.handleParentInfo(e)} value={this.state.lastname} />
+            onChange={ this.addParentInfo } value={this.state.lastname} />
 					</fieldset>
 
 					<fieldset className='form-group'>
 						<label>Email:</label>
 
 						<input id='formEmail' className='form-input' type='text' 
-            onChange={(e) => this.state.handleParentInfo(e)} value={this.state.email} />
+            onChange={ this.addParentInfo } value={this.state.email} />
 					</fieldset>
 
 					<fieldset className='form-group'>
 						<label>Address:</label>
 
 						<input id='formAddress' className='form-input' type='text' 
-            onChange={(e) => this.state.handleParentInfo(e)} value={this.state.address} />
+            onChange={ this.addParentInfo } value={this.state.address} />
 					</fieldset>
 
 					<fieldset className='form-group'>
 						<label>city:</label>
 
 						<input id='formCity' className='form-input' type='text' 
-            onChange={(e) => this.state.handleParentInfo(e)} value={this.state.city} />
+            onChange={ this.addParentInfo } value={this.state.city} />
 					</fieldset>
 
 					<fieldset className='form-group'>
 						<label>State:</label>
 
 						<input id='formState' className='form-input' type='text' 
-            onChange={(e) => this.state.handleParentInfo(e)} value={this.state.state} />
+            onChange={ this.addParentInfo } value={this.state.state} />
 					</fieldset>
 
 					<fieldset className='form-group'>
 						<label>Zip:</label>
 
 						<input id='formZip' className='form-input' type='text' 
-            onChange={(e) => this.state.handleParentInfo(e)} value={this.state.zip} />
+            onChange={ this.addParentInfo } value={this.state.zip} />
 					</fieldset>
 
 					<fieldset className='form-group'>
 						<label>Phone:</label>
 
 						<input id='formPhone' className='form-input' type='text' 
-            onChange={(e) => this.state.handleParentInfo(e)} value={this.state.phone} />
+            onChange={ this.addParentInfo } value={this.state.phone} />
 					</fieldset>
 
 				</form>
 			</div>
 
 
-
-
-        {/* <div>
-          <h1>Please register for Student (Dancer)</h1>
-          </div>
-
-          <div id="profile-form">
-				<p>{this.props.error}</p>
-
-	
-				<form className='react-form'>
-
-					<fieldset className='form-group'>
-						<label >First Name:</label>
-
-						<input id='formFirstName' className='form-input' name='firstname' type='text' onChange={(e) => this.props.handleChange(e)} value={this.props.firstname} />
-					</fieldset>
-
-					<fieldset className='form-group'>
-						<label>Last Name:</label>
-
-						<input id='formLastName' className='form-input' name='lastname' type='text' onChange={(e) => this.props.handleChange(e)} value={this.props.lastname} />
-					</fieldset>
-
-					<fieldset className='form-group'>
-						<label>Age:</label>
-
-						<input id='formEmail' className='form-input' name='age' type='text' onChange={(e) => this.props.handleChange(e)} value={this.props.email} />
-					</fieldset>
-
-					<fieldset className='form-group'>
-						<label>Gender:</label>
-
-						<input id='formAddress' className='form-input' name='gender' type='text' onChange={(e) => this.props.handleChange(e)} value={this.props.address} />
-					</fieldset>
-
-					<fieldset className='form-group'>
-						<label>Class</label>
-
-						<input id='formCity' className='form-input' name='class' type='text' onChange={(e) => this.props.handleChange(e)} value={this.props.city} />
-					</fieldset>
-
-			
-				</form>
-			</div> */}
-
-
-
          <h1>Maximum Dance</h1>
          <div className='parentContainer'>
              <h4>Parent Information:</h4>
                  {this.props.parent ? <img className='avatar' src={this.props.parent.img} alt='' /> : null }
-             {/* <div>
+                
+                <div>
                  <p>First Name: { this.props.parent ? this.props.parent.firstname : null }</p>
                  <p>Last Name: { this.props.parent ? this.props.parent.lastname : null }</p>
-                 <p>Email: { this.props.parent ? this.props.parent.email : null }</p>
-                 <p>Address: { this.props.parent ? this.props.parent.address : null }</p>
-                 <p>City: { this.props.parent ? this.props.parent.city : null }</p>
-                 <p>State: { this.props.parent ? this.props.parent.state : null }</p>
-                 <p>Zip: { this.props.parent ? this.props.parent.zip : null }</p>
-                 <p>Phone: { this.props.parent ? this.props.parent.phone : null }</p>
-             </div> */}
+                </div>
 
                  <a href={process.env.REACT_APP_LOGOUT}><button>Logout</button></a>
          </div>
@@ -230,8 +146,5 @@ function mapStateToProps(state){
   }
 }
 
-let parentActions = {
-  getParentInfo: getParentInfo
-}
 
-export default connect(mapStateToProps, parentActions)(Dashboard);
+export default connect( mapStateToProps, { getParentInfo, addParentInfo } )(Dashboard);
--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 //SET INITIAL STATE
 const initialState = {
     parent: {}
+
 }
 
 //ACTION TYPES
 const GET_PARENT_INFO = "GET_PARENT_INFO";
+const ADD_PARENT_INFO = "ADD_PARENT_INFO";
 
 //ACTION CREATORS
 export function getParentInfo() {
@@ -19,12 +21,25 @@ export function getParentInfo() {
     }
 }
 
+export function addParentInfo(obj){
+    const newParentInfo = axios.post('db', obj).then( res => {
+        return res.data
+    })
+    return {
+        type: ADD_PARENT_INFO,
+        payload: newParentInfo
+    }
+}
+
 //REDUCER FUNCTION
 export default function reducer(state = initialState, action) {
     switch(action.type) {
         case GET_PARENT_INFO + '_FULFILLED':
-            return Object.assign({}, state, { parent: action.payload })
-        
+            return Object.assign({}, state, { parent: action.payload });
+
+        case ADD_PARENT_INFO + '_FULFILLED':
+            return Object.assign({}, state, { parent: action.payload });
+
         default:
             return state;
     }
